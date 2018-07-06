b956b888a15cc23259ba2783f5e3e6e76f94b259
Victor Velasquez <victorv24@gmail.com>
2018-07-05 22:32:48 -0700

    Questions 1 - 3 are complete

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
-function averagePrice(item) {
-	return (item === "price")
-}
+// Question 1 — Find the Average Price:
+// Get the sum of the price properties inside the items array
+// and divide that by the number of price indexes there are.
 
-let avgPrice = items.filter(averagePrice(items[0]))
-
-/*
-I need to find the average price of all of the items inside of the ITEMS array. 
-This means I need to take each price and add them up. 
-After that, I need to divide that total by the number of items there are.
-I can use ".length" have JS count the total for me.
-
-The ITEMS array has objecst within it that have the 'price' property inside of it. 
-To get that I need to first go into the ITEMS array, and then to into the objects one by one.
-Then I need to say, "grab the PRICE property from this object".
-Then use that to add the prices together.
-Then say now divide this by ITEM/OBJECT/PRICE.length
-*/
-
-console.log(avgPrice)
-
-
-// Answers displayed to document
+let avgPrice = (items.reduce(function (priceIndex1, priceIndex2) { 
+	return priceIndex1 + priceIndex2.price
+}, 0)) / items.length
 
 const answer1 = document.querySelector('#answer1')
 answer1.innerHTML = `The average price is $${avgPrice}`
+
+
+// Question 2 — Filter and Map:
+// Filter through the array for items between $14 and $18, 
+// map through those items to produce their titles, then 
+// join the arrays to procuce clean output.
+
+let priceRange = items.filter(function (priceIndex) {
+	return priceIndex.price > 14 && priceIndex.price < 18
+}).map(function (titleIndex) {
+	return titleIndex.title
+}).join("\n")
+
 const answer2 = document.querySelector('#answer2')
-answer2.innerHTML = ``
+answer2.innerHTML = `${priceRange}`
+
+
+// Question 3 — Filter and Print:
+// Filter through the array to find the item with the "GBP" currency code
+// and print that item's title and price to the page.
+
+let currencyCode = items.filter(function (code) {
+	return code.currency_code === "GBP"
+}).map(function (titleIndex) {
+	return titleIndex.title + " costs &#163;" + titleIndex.price
+})
+
 const answer3 = document.querySelector('#answer3')
-answer3.innerHTML = ``
-const answer4 = document.querySelector('#answer4')
-answer4.innerHTML = ``
-const answer5 = document.querySelector('#answer5')
-answer5.innerHTML = ``
-const answer6 = document.querySelector('#answer2')
-answer6.innerHTML = ` were made by their sellers`
+answer3.innerHTML = `${currencyCode}`
+
+
+
+// const answer4 = document.querySelector('#answer4')
+// answer4.innerHTML = ``
+
+// const answer5 = document.querySelector('#answer5')
+// answer5.innerHTML = ``
+
+// const answer6 = document.querySelector('#answer2')
+// answer6.innerHTML = `${} were made by their sellers`
